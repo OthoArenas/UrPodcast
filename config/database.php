3480f42d72079080d9c456038ac9e07e5c9937c7
Othoniel Eduardo Salazar Arenas <othosalazar@gmail.com>
2020-02-23 22:41:58 -0600

    PostgressDB connection

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATABASE_URL = parse_url('postgres://bupkfksfufxmwz:1787daf8afd2aac327c6f6810e2c580bd80868e67234c3b6f29929b12b046b2c@ec2-52-202-185-87.compute-1.amazonaws.com:5432/d7bdgmrkaamlln');
+
 use Illuminate\Support\Str;
 
 return [
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pas"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
